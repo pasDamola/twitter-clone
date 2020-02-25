98a3a8ef091f941d758422b3e515ea541b2d2d6f
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-02-25 12:06:14 +0100

    Completed tablet view

---
+++ components/navbar-right.vue
@@ -0,0 +1,169 @@
+<template>
+  <v-navigation-drawer
+    permanent
+    fixed
+    right
+    width="350"
+    color="dark"
+    app
+  >
+    <v-list>
+      <v-list-item>
+        <v-input
+          :hide-details="true"
+          class="twitter-search"
+        >
+          <v-icon slot="prepend" color="rgba(255, 255, 255, 0.5)">
+            mdi-magnify
+          </v-icon>
+          Search Twitter
+        </v-input>
+      </v-list-item>
+      <v-list-item>
+        <v-list width="100%" color="rgb(30, 30, 30)" class="mt-5 trends">
+          <v-list-item>
+            <v-layout>
+              <h3>Trends</h3>
+              <v-spacer />
+              <img src="/icons/settings_blue.svg" alt="">
+            </v-layout>
+          </v-list-item>
+          <v-divider />
+          <div
+            v-for="(item, index) in trending"
+            :key="index"
+          >
+            <v-list-item
+              v-if="!item.divider"
+              two-line
+            >
+              <v-list-item-content>
+                <v-list-item-title>
+                  <v-layout class="caption" justify-space-between>
+                    <span>{{ index + 1 }} <span>.</span> {{ item.tag }} <span v-show="item.tag">.</span> Trending </span>
+                    <v-icon>mdi-chevron-down</v-icon>
+                  </v-layout>
+                </v-list-item-title>
+                <v-list-item-subtitle class="trend-name">
+                  {{ item.name }}
+                </v-list-item-subtitle>
+                <v-list-item-subtitle class="caption">
+                  {{ formatNumber(item.tweetsCount) }} Tweets
+                </v-list-item-subtitle>
+                <v-list-item-subtitle v-if="item.people" class="caption">
+                  <v-layout align-start="">
+                    <v-avatar size="20">
+                      <img src="https://randomuser.me/api/portraits/women/81.jpg">
+                    </v-avatar>
+                    <p class="caption">
+                      Rey rey and React Developer are Tweeting about this
+                    </p>
+                  </v-layout>
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider v-else />
+          </div>
+
+          <v-divider />
+          <v-list-item>
+            <p class="more">
+              Show more
+            </p>
+          </v-list-item>
+        </v-list>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    trending: [
+      {
+        name: '#Ooin',
+        tweetsCount: 6423,
+        tag: 'Sports',
+        people: true
+      },
+      { divider: true },
+      {
+        name: '#TuesdayMotivation',
+        tweetsCount: 22400,
+        tag: '',
+        people: true
+      },
+      { divider: true },
+      {
+        name: '#TuesdayThoughts',
+        tweetsCount: 31353,
+        tag: ''
+      },
+      { divider: true },
+      {
+        name: '#NapVar',
+        tweetsCount: 23850,
+        tag: 'Sports'
+      },
+      { divider: true },
+      {
+        name: '#EndSarsNow',
+        tweetsCount: 2690,
+        tag: 'Politics'
+      }
+    ]
+  }),
+  methods: {
+    formatNumber(num) {
+      if (typeof num === 'number') {
+        if (num < 10000) {
+          return num.toLocaleString();
+        } else {
+          return `${Math.sign(num) * ((Math.abs(num) / 10000).toFixed(1))}K`;
+        }
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+  .twitter-search::v-deep {
+    align-items: center;
+    background-color: rgba(30, 30, 30);
+    padding: 5px 15px;
+    font-size: 0.8rem;
+    color: rgba(255, 255, 255, 0.5);
+    border-radius: 50px;
+    .v-input__slot {
+      margin-bottom: 0;
+    }
+  }
+
+  .trends {
+    border-radius: 17px;
+  }
+
+  p.caption {
+    margin: 0 5px;
+    white-space: normal
+  }
+
+  .caption {
+    span, i {
+      color: rgba(255, 255, 255, 0.5);
+    }
+  }
+
+  .trend-name {
+    font-size: 16px;
+    font-weight: bold;
+    color: #fff !important;
+  }
+
+  .more {
+    color: #1da1f2;
+    margin: 10px 0;
+  }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,17 +2,20 @@
   <v-app>
     <mobile-layout v-if="isMobile" class="mobile-layout" />
     <tablet-layout v-else-if="isTablet" class="tablet-layout" />
+    <desktop-layout v-else />
   </v-app>
 </template>
 
 <script>
 import MobileLayout from '@/layouts/mobile-layout';
 import TabletLayout from '@/layouts/tablet-layout';
+import DesktopLayout from '@/layouts/desktop-layout';
 export default {
-  components: { MobileLayout, TabletLayout },
+  components: { MobileLayout, TabletLayout, DesktopLayout },
   data: () => ({
     isMobile: false,
     isTablet: false
+    // isDesktop: false
   }),
   beforeMount() {
     /* eslint-disable-next-line */
@@ -21,23 +24,21 @@ export default {
       window.onload = this.onLoad;
     }
   },
+  beforeDestroy() {
+    if (typeof window !== 'undefined') {
+      window.removeEventListener('resize', this.onResize);
+    }
+  },
   methods: {
     onLoad(e) {
-      if (e.currentTarget.innerWidth < 500) {
-        this.isMobile = true;
-      } else if (e.currentTarget.innerWidth > 500 && e.currentTarget.innerWidth < 1005) {
-        this.isMobile = false;
-        this.isTablet = true;
-      }
+      this.isMobile = e.currentTarget.innerWidth < 500;
+      this.isTablet = e.currentTarget.innerWidth > 500 && e.currentTarget.innerWidth < 1005;
+      // this.isDesktop = e.currentTarget.innerWidth > 1005;
     },
     onResize(e) {
       this.isMobile = e.target.innerWidth < 500;
       this.isTablet = e.target.innerWidth > 500 && e.target.innerWidth < 1005;
-    }
-  },
-  beforeDestroy() {
-    if (typeof window !== 'undefined') {
-      window.removeEventListener('resize', this.onResize);
+      // this.isDesktop = e.target.innerWidth > 1005;
     }
   }
 };
